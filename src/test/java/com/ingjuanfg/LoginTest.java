b278ddc8ddfdee1ff2b81115e19742b53efeb7f7
ingjuanfg <juan.isfg@gmail.com>
2024-09-22 14:30:07 -0500

    Selenium usando driver local

--- src/test/java/com/ingjuanfg/LoginTest.java
+++ src/test/java/com/ingjuanfg/LoginTest.java
@@ -1,7 +1,5 @@
 package com.ingjuanfg;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,18 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
 
-    WebDriver driver;
-
-    @BeforeEach
-    public void configuracionInicial(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
     @Test
     public void testLoginExitoso() {
         //ARRANGE
+        System.setProperty("webdriver.chrome.driver", "C://tools//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+
         //ACT
         WebElement inputUser = driver.findElement(By.id("user-name"));
         WebElement inputPassword = driver.findElement(By.id("password"));
@@ -31,13 +25,10 @@ public class LoginTest {
         inputUser.sendKeys("standard_user");
         inputPassword.sendKeys("secret_sauce");
         buttonLogin.click();
+
         //ASSERT
         WebElement pageTitle = driver.findElement(By.xpath("//span[@data-test='title']"));
         assertEquals("Products", pageTitle.getText());
-    }
-
-    @AfterEach
-    public void configuracionFinal(){
         driver.close();
     }
 }
